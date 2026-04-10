673f8bb0dee89b287f2f99dee18584ca417836c0
Caros Emmanuelli <ceemmanuellis@gmail.com>
2026-04-10 08:16:58 +0200

    New repo

---
+++ library/views.py
@@ -0,0 +1,106 @@
+import json
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+from django.views.decorators.csrf import csrf_exempt
+from .models import LibraryEntry
+
+@require_GET
+def health(request):
+    return JsonResponse({"status": "0ok"})
+
+## Error function
+def error(details):
+    return JsonResponse({
+        "error": "validation_error",
+        "message": "Invalid data introduced",
+        "details": { "field" : details }
+    }, status = 400)
+
+# We make a function to refer to any game that alreaddy exists in the DB.
+def duplicated_error(details):
+    return JsonResponse({
+        "error": "duplicate_entry",
+        "message": "That game already exists",
+        "details": { details: "duplicate"}
+    })
+
+# We add a game to the program.
+@csrf_exempt
+def add_game(request):
+
+    ## Check the request
+    if request.method != "POST":
+        return JsonResponse({"error": "Method not allowed"}, status=405)
+    
+    # get all data
+    data = json.loads(request.body)
+
+    # Assiing the data to a variable 
+    external_game_id = data.get("external_game_id")
+    status = data.get("status")
+    hours_played = data.get("hours_played")
+    
+    #Check if the id is in correct format and catch the exception.
+    try:
+        if not (isinstance(external_game_id, str)):
+            return error("Invalid id")
+        if LibraryEntry.objects.filter(external_game_id = external_game_id).exists():
+            return duplicated_error("Id already exists")
+        if external_game_id == "":
+            return error("Not allowed null values")
+    except Exception as e:
+        JsonResponse({
+            "error": str(e)
+        }, status = 400)
+    
+    #check if the status is in the correct format and catch the exception
+    try:
+        if not (status.lower() == "playing" or status.lower() == "completed" or status.lower() == "whishlist" or status.lower() == "dropped"):
+            return error("Invalid status")
+        if status == "":
+            return error("Not allowed null values")
+    except Exception as e:
+        JsonResponse({
+            "error": str(e)
+        }, status = 400)
+    
+    #Check the hours played correct format and catch the exception
+    try:
+        hours_played = int(hours_played)
+        if hours_played is None or str(hours_played) == "":
+            return error("must be numbers")
+        if hours_played < 0:
+            raise ValueError
+
+    except (TypeError, ValueError) as e:
+        return JsonResponse({
+            "error": str(e)
+        }, status=400)
+    
+    #to create a dictionary and finaly show it 
+    entry = LibraryEntry.objects.create(
+        external_game_id = external_game_id,
+        status = status.lower(),
+        hours_played = hours_played
+    )
+
+    # The JSON response to show the info.
+    return JsonResponse({
+        "id": entry.external_game_id,
+        "status": entry.status,
+        "hours_playes": entry.hours_played
+    }, status = 201)
+
+
+@require_GET
+def get_games(request):
+    #to create a dictionary and finaly show it 
+    entry = LibraryEntry.objects.all()
+    d_games = {}
+    
+    for l in entry:
+        d_games.append({
+            "external_game_id": l.external_game_id,
+            "status": l.status,
+            "hours_played": l.hours_played
+        })
