219b37cfc29f0fb45df4eafa930cf703538087a7
Caros Emmanuelli <ceemmanuellis@gmail.com>
2026-04-15 11:59:10 +0200

    changes in settings

--- steamlike_backend/settings.py
+++ steamlike_backend/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Corsheaders",
+    "tus_apps",
 
     # Third-party.
     "corsheaders",
@@ -40,12 +42,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
